5e3943310c9ed1f3cda0aed1ad7f9e03b3ee3fe2
sufiyanirfan3 <sufiyanirfan1613@gmail.com>
2023-03-02 16:41:16 +0500

    changes in backend prompt

--- main.js
+++ main.js
@@ -331,27 +331,25 @@ $(function () {
     console.log(jobTitle1)
 
     prompt = `
-    Write a ergonomic evaluation report, provide recommendations and 
-implementation of ergonomic interventions. This is a report that an ergonomist or safety advisor is 
-writing for their company managers.\n\n
+    Write a job task physical demands evaluation report, provide recommendations and implementation of ergonomic interventions. This is a report that an ergonomist or safety advisor is writing for their company senior management.Include recommendation and implementation and any applicable Canadian musculoskeletal workplace injury statistics for the industry.\n\n
 Enter information about the employee\n
-Name: ${name}\n
+Employee Name: ${name}\n
 Job title and short job description : ${jobTitle} \n
 Organization working for: ${organization} \n
 Job site: ${jobSite} \n
-Length of Time on the Job: ${jobExp} \n
+Length of Time working in this position: ${jobExp} \n
 Physical Demands including any risk factors: ${physicalDemands} \n
 Environmental factors: ${envFactors} \n
 Job Schedule and Breaks: ${jobSchedule} \n
-What was the REBA score using Vergo? ${rebaScore}\n
+What was the REBA score using Vergo? And explain the reba score ${rebaScore}\n
 
 Enter information about the evaluator\n
 The person who is conducting the ergonomic assessment: ${name1}\n
-Job title: ${jobTitle1}\n
+The evaluators job title: ${jobTitle1}\n
 When was the job task assessed using Vergo?\n
 Job task was assessed on ${jobDate} for hazards related to musculoskeletal disorders, as well as 
-other issues pertaining to workplace health and safety. Assessment took place by using a computer 
-vision pose estimation ergonomic tool that uses REBA standard.\n 
+other issues pertaining to workplace health and safety. Assessment took place by using Vergo, a computer vision ergonomic tool that uses REBA standard.\n
+
 
     `
 
